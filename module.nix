d131089cac0b158e6a97d49ab4f622826ed29e42
Lemm1 <mrlolvovgres@gmail.com>
2025-03-15 16:40:55 +0200

    Fixed type as per comment in PR

--- module.nix
+++ module.nix
@@ -10,7 +10,7 @@
     builtins.concatStringsSep "\n" (builtins.map (name: builtins.readFile (dir + "/${name}")) (builtins.attrNames (builtins.readDir dir)));
 in {
   options.programs.droidux = {
-    enable = lib.mkEnableOption "Weather to install droidux and setup adb, udev";
+    enable = lib.mkEnableOption "Whether to install droidux and setup adb, udev";
     package = lib.mkOption {
       type = with lib.types; nullOr package;
       default = pkgs.callPackage ./pkg {};
